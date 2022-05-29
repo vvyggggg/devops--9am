a38a830f5e9627a3488e8aa90334d9f158b98315
vvyggggg <102725056+vvyggggg@users.noreply.github.com>
2022-05-30 00:51:26 +0530

    commit changes

---
+++ #string.py
@@ -0,0 +1,49 @@
+#String
+a="keys"
+print(a)
+print(type(a))
+
+b='lorry'
+print(b)
+print(type(b))
+
+c=''' raki is my 
+hero
+'''
+print(type(c))
+print(c)
+d=''' ''' '''lassi is 
+my friend''' ''' '''
+print(type(d))
+print(d)
+e='how \n are you'
+print(e)
+f='how \t are you'
+print(f)
+g="ok 'iamfine' now"
+print(g)
+h='ok "ready to mingle"lok'
+print(h)
+str1="raju"
+str2="raju"
+print(id(str1))
+print(id(str2))
+str3="loasi"
+
+
+
+
+print(str3)
+
+for i in str3:
+     print(i)
+string4='killl'
+print(len(string4))
+print(string4*6)
+print(string4[0:4]*8)
+string5='lilly'
+string6=string4+string5
+print(string6)
+print("hllo"+string5+"io")
+print("jikkk",string5)
+print(len(string4))
