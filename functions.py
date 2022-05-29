92c3d35f062fc2015f225e87c28c2c70cfe61322
vvyggggg <102725056+vvyggggg@users.noreply.github.com>
2022-05-30 01:39:44 +0530

    ok

---
+++ functions.py
@@ -0,0 +1,17 @@
+string1="happy"
+string2="Local"
+string3="KILL"
+a=string1.swapcase()
+print(a)
+b=string1.istitle()
+print(b)
+C=string2.swapcase()
+print(C)
+d=string2.istitle()
+print(d)
+f=string3.swapcase()
+print(f)
+g=string3.istitle()
+print(g)
+
+
